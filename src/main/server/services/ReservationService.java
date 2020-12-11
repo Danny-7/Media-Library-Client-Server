8056ba454809911ecf4be57bd62d6d976a3f8070
Jules Doumèche <30329843+julio4@users.noreply.github.com>
2020-12-11 11:45:06 +0100

    refactor services IO

--- src/main/server/services/ReservationService.java
+++ src/main/server/services/ReservationService.java
@@ -15,54 +15,15 @@ public class ReservationService extends LibraryService implements Runnable{
     @Override
     public void run() {
         try {
-            send("Enter your subscriber number please ?");
-
-            String read;
-            do {
-                read = read().toString();
-                System.out.println("read : " + read);
-            } while(!isInteger(read));
-
-            int subNumber = Integer.parseInt(read);
-//            int subNumber = (int) read();
-
-            Subscriber sb = findSubscriber(subNumber);
-            if(sb == null)
-                throw new IllegalAccessException("The subscriber doesn't exist !");
-
-            send("Enter a document number please ?");
-
-            do {
-                read = read().toString();
-            } while(!isInteger(read));
-            send("read: " + read);
-
-            int docNumber = Integer.parseInt(read);
-//            int docNumber = (int) read();
-
-            GeneralDocument doc = findDocument(docNumber);
-            if(doc == null)
-                throw new IllegalAccessException("The document doesn't exist !");
+            Subscriber sb = requestSubscriber();
+            GeneralDocument doc = requestDocument();
 
             doc.reservationFor(sb);
 
             send("The document : " + doc + " has been successfully reserved");
 
-        } catch(IllegalAccessException | BorrowException e1 ) {
+        } catch(BorrowException e1) {
             send("error : " + e1.getMessage());
         }
-    }
-
-    private static boolean isInteger(String str)
-    {
-        try
-        {
-            int i = Integer.parseInt(str);
-        }
-        catch(NumberFormatException nfe)
-        {
-            return false;
-        }
-        return true;
     }
 }
