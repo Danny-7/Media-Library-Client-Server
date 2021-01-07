001ee0582d8d482bc97143d92b10e737d015b64c
julio4 <jules.doumeche@gmail.com>
2021-01-07 14:36:49 +0100

    Added Test

--- src/tests/models/DVDBorrowReservationTest.java
+++ src/tests/models/DVDBorrowReservationTest.java
@@ -6,6 +6,7 @@ import main.server.models.exceptions.BorrowException;
 import main.server.models.exceptions.ReservationException;
 import main.server.models.members.Subscriber;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
@@ -20,51 +21,107 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class DVDBorrowReservationTest {
 
-    private static Subscriber sb;
+    private static Subscriber i_sb;
+    private static Subscriber s_sb;
+    private static GeneralDocument doc;
 
     @BeforeAll
     static void subscriber() {
-        sb = new Subscriber("Daniel", LocalDate.of(2001,7,24));
+        i_sb = new Subscriber("Initial Subscriber", LocalDate.of(2001,7,24));
+        s_sb = new Subscriber("Second Subscriber", LocalDate.of(1997,6,2));
+    }
+
+    @BeforeEach
+    public void document() {
+        doc = new DVD("Le roi lion",3);
+    }
+
+    @Test
+    public void testInitialDocState() {
+        assertTrue(doc.isAvailable());
+        assertNull(doc.getHolder());
     }
 
     @Test
     public void testReservation() {
-        GeneralDocument doc = new DVD("Le roi lion",3);
-        assertTrue(doc.isAvailable());
-        assertNull(doc.getHolder());
-        doc.reservationFor(sb);
-        assertEquals(sb, doc.getHolder());
+        doc.reservationFor(i_sb);
+        assertEquals(i_sb, doc.getHolder());
         assertTrue(doc.isReserved());
     }
 
     @Test
+    public void testBorrow() {
+        doc.borrowBy(i_sb);
+        assertEquals(i_sb, doc.getHolder());
+        assertTrue(doc.isBorrowed());
+    }
+
+    @Test
     public void testBorrowWithReservationBefore() {
-        GeneralDocument doc = new DVD("Le roi lion",20);
-        assertTrue(doc.isAvailable());
-        assertNull(doc.getHolder());
-        assertThrows(ReservationException.class, () -> {
-            doc.reservationFor(sb);
-                });
+        doc.reservationFor(i_sb);
+        assertTrue(doc.isReserved());
+        doc.borrowBy(i_sb);
+        assertTrue(doc.isBorrowed());
+    }
+
+    @Test
+    public void testReservationBorrowForDvdDate() {
+        GeneralDocument adult_doc = new DVD("Le roi lion",20);
         // we can't reserve a dvd when our age is smaller than the recommended
-        assertNull(doc.getHolder());
+        assertThrows(ReservationException.class, () -> {
+            adult_doc.reservationFor(i_sb);
+        });
+        assertNull(adult_doc.getHolder());
+        assertFalse(adult_doc.isReserved());
 
         assertThrows(BorrowException.class, () -> {
-            doc.borrowBy(sb);
+            adult_doc.borrowBy(i_sb);
         });
+        // Doc still in initial state
+        assertNull(adult_doc.getHolder());
+        assertFalse(adult_doc.isBorrowed());
+    }
 
-        // we can't borrow a dvd when our age is smaller than the recommended
-        assertFalse(doc.isBorrowed());
+    @Test
+    public void testBorrowWithReservationBeforeWithDifferentSubscriber() {
+        // we can't borrow if another subscriber reserved the doc
+        doc.reservationFor(i_sb);
+        assertTrue(doc.isReserved());
+        assertThrows(BorrowException.class, () -> {
+            doc.borrowBy(s_sb);
+        });
+        assertTrue(doc.isReserved());
+    }
+
+    @Test
+    public void testBorrowBorrowedDoc() {
+        doc.borrowBy(i_sb);
+        assertThrows(BorrowException.class, () -> {
+            doc.borrowBy(i_sb);
+        });
+        assertThrows(BorrowException.class, () -> {
+            doc.borrowBy(s_sb);
+        });
+    }
+
+    @Test
+    public void testReservationBorrowedDoc() {
+        doc.borrowBy(i_sb);
+        assertThrows(ReservationException.class, () -> {
+            doc.reservationFor(i_sb);
+        });
+        assertThrows(ReservationException.class, () -> {
+            doc.reservationFor(s_sb);
+        });
     }
 
     @Test
     public void testReturnBack() {
-        GeneralDocument doc = new DVD("Le petit chaperon rouge",3);
-        doc.borrowBy(sb);
-
-        // we can reserve a dvd when our age is higher than the recommended
-        assertEquals(sb,doc.getHolder());
-        assertFalse(doc.isBorrowed());
+        doc.borrowBy(i_sb);
+        doc.returnBack();
+        // Doc still in initial state
+        assertTrue(doc.isAvailable());
+        assertNull(doc.getHolder());
     }
 
-    
 }
